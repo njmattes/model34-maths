0a27543bca54ee5e73ac06c0fbd3800c68953a79
Nathan Matteson <matteson@obstructures.org>
2020-02-24 08:32:19 -0600

    Calculate NeckSection form multiple NeckArea objects

--- model34_maths/neck_section.py
+++ model34_maths/neck_section.py
@@ -1,136 +1,55 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from math import sqrt, atan
-from model34_maths.neck_ellipse import NeckEllipse
-from model34_maths.config import BODY_DEPTH, BODY_WIDTH
-from model34_maths.config import NUT_DEPTH, NUT_WIDTH
-from model34_maths.config import RAIL_WIDTH, SCALE_LENGTH
-from model34_maths.config import NECK_THICKNESS, FRETBOARD_THICKNESS
-from model34_maths.decorators import finite_method
+from model34_maths.neck_area import NeckArea
+from model34_maths.neck_curve import NeckCurve
 
 
 class NeckSection(object):
-    def __init__(self, neck_position, min_outer_a=None, max_outer_a=None,
-                 min_inner_a=None, max_inner_a=None):
-        """Section of neck made up of simple(r) shapes.
-
-        #TODO: Finish docstring
-        :param neck_position: position along neck, 0 <= x <= neck_length
-        :type neck_position: float
-        :param min_inner_a:
-        :type min_inner_a: float
-        :param max_inner_a:
-        :type max_inner_a: float
-        :param min_outer_a:
-        :type min_outer_a: float
-        :param max_outer_a:
-        :type max_outer_a: float
-        """
-        self.neck_position = neck_position
-        neck_position = neck_position / (SCALE_LENGTH / 2)
-        self.width = NUT_WIDTH + neck_position * (BODY_WIDTH - NUT_WIDTH)
-        self.depth = NUT_DEPTH + neck_position * (BODY_DEPTH - NUT_DEPTH)
-        self.inner = NeckEllipse(
-            self.width - RAIL_WIDTH, self.depth - NECK_THICKNESS,
-            FRETBOARD_THICKNESS,
-            a=None if min_inner_a is None else
-            min_inner_a + (max_inner_a - min_inner_a) * neck_position)
-        self.outer = NeckEllipse(
-            self.width, self.depth, 0,
-            a=None if min_outer_a is None else
-            min_outer_a + (max_outer_a - min_outer_a) * neck_position)
+    def __init__(self, areas):
+        self.areas = areas
+        self._area = None
         self._y_bar = None
-        self._second_moment_area = None
+        self._first_moment = None
+        self._second_moment = None
+        self.x_min = min([area.x_min for area in self.areas])
+        self.x_max = max([area.x_max for area in self.areas])
+
+    @property
+    def area(self):
+        if self._area is None:
+            return 2 * sum([_.area for _ in self.areas])
+        return self._area
 
     @property
     def y_bar(self):
-        """y-bar. y component of centroid. (x component is assumed to be 0).
-
-        :return: y-bar
-        :rtype: float
-        """
         if self._y_bar is None:
             self._y_bar = (
-                    self.get_integral_yda_between_ellipses(self.inner.width) /
-                    (2 * self.get_integral_da_between_ellipses(self.inner.width)))
+                    sum([_.area * _.y_bar for _ in self.areas]) /
+                    sum([_.area for _ in self.areas]))
         return self._y_bar
 
     @property
-    def second_moment_area(self):
-        if self._second_moment_area is None:
-            self._second_moment_area = self.get_second_moment_area(
-                self.inner.width)
-        return self._second_moment_area
+    def first_moment(self):
+        if self._first_moment is None:
+            pass
+        return self._first_moment
 
-    def get_integral_da_between_ellipses(self, x):
-        """Integral of dA in the denominator of x-bar and y-bar.
-
-        y = k - a * sqrt(b**2 - x**2) / b
-        dA = [y of inner ellipse] - [y of outer ellipse]
-
-        Integral( (ki - ai * sqrt(bi**2 - x**2) / bi) -
-                  (ko - ao * sqrt(bo**2 - x**2) / bo) )
-
-        integrate (k - b * sqrt(a^2-x^2)/a) - (K - B * sqrt(A^2-x^2)/A) dx
-
-        :param x:
-        :type x:
-        :return:
-        :rtype:
-        """
-        a = self.inner.a
-        A = self.outer.a
-        b = self.inner.b
-        B = self.outer.b
-        k = self.inner.k
-        K = self.outer.k
-        eq = (
-            -b * x * sqrt(a**2 - x**2) / (2 * a) +
-            (a * b * atan(x * sqrt(a**2 - x**2) / (x**2 - a**2))) / 2 +
-            B * x * sqrt(A**2 - x**2) / (2 * A) -
-            (A * B * atan(x * sqrt(A**2 - x**2) / (x**2 - A**2))) / 2 +
-            k * x - K * x
-        )
-        return eq
-
-    def get_integral_yda_between_ellipses(self, x):
-        """Integral of y * dA in the numerator of y-bar.
-
-        :param _x:
-        :type _x:
-        :return:
-        :rtype:
-
-        integrate ((k - b * sqrt(a^2-x^2)/a) - ((k - b * sqrt(a^2-x^2)/a) - (K - B * sqrt(A^2-x^2)/A)) / 2) * ((k - b * sqrt(a^2-x^2)/a) - (K - B * sqrt(A^2-x^2)/A)) dx
+    @property
+    def second_moment(self):
+        if self._second_moment is None:
+            self._second_moment = sum([
+                2 * (_.second_moment + _.area * (_.y_bar - self.y_bar) ** 2)
+                for _ in self.areas])
+        return self._second_moment
 
 
-        """
-        a = self.inner.a
-        A = self.outer.a
-        b = self.inner.b
-        B = self.outer.b
-        k = self.inner.k
-        K = self.outer.k
-        eq = (-1 / (6 * a**2 * A**2)) * (
-                -3 * a**2 * A**2 * b**2 * x + 3 * a * A**2 * b * k * x *
-                sqrt(a**2 - x**2) + 3 * a**2 * A**2 * B**2 * x -
-                3 * a**2 * A * B * K * x * sqrt(A**2 - x**2) -
-                3 * a**2 * A**2 * k**2 * x +
-                3 * a**2 * A**2 * K**2 * x -
-                3 * a**2 * A**3 * B * K * atan(x / sqrt(A**2 - x**2)) -
-                a**2 * B**2 * x**3 +
-                3 * a**3 * A**2 * b * k * atan(x / sqrt(a**2 - x**2)) +
-                A**2 * b**2 * x**3)
-        return eq
-
-    @finite_method(10000, 0)
-    def get_second_moment_area(self, x):
-        a = self.inner.a
-        A = self.outer.a
-        b = self.inner.b
-        B = self.outer.b
-        k = self.inner.k
-        K = self.outer.k
-        Y = self.y_bar
-        return (((k - b * sqrt(a ** 2 - x ** 2) / a) +
-                 (K - B * sqrt(A ** 2 - x ** 2) / A)) / 2 - Y) ** 2
+if __name__ == '__main__':
+    y0 = NeckCurve(1, 1, 0, 2)
+    y1 = NeckCurve(.75, .75, 0, 2)
+    area0 = NeckArea(0, .75, y0, y1)
+    y1 = NeckCurve(0, 0, 0, 0)
+    area1 = NeckArea(.75, 1, y0, y1)
+    section = NeckSection([area0, area1])
+    print(section.area)
+    print(section.y_bar)
+    print(section.second_moment)
