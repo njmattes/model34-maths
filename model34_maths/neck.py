647bd4455921e82535814d617d4ddfcafb679e89
Nathan Matteson <matteson@obstructures.org>
2020-02-29 17:36:50 -0600

    Methods for building sections and b's

--- model34_maths/neck.py
+++ model34_maths/neck.py
@@ -1,64 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from math import sqrt, atan, log
 import numpy as np
+from scipy.integrate import dblquad
 from numpy.polynomial.polynomial import polyfit
 from model34_maths.neck_section import NeckSection
 from model34_maths.neck_area import NeckArea
 from model34_maths.neck_curve import NeckCurve
-from model34_maths.config import SCALE_LENGTH, NeckConfig
+from model34_maths.config import NeckConfig
 
 
 class Neck(NeckConfig):
-    def __init__(self, n):
+    def __init__(self, a_min=1.2, a_max=2.5, n=10):
         """Neck made up of n NeckSections.
 
         """
 
-        minor_as = self.get_random_in_range(self.widths * 1.1,
-                                            self.widths * 2)
-        minor_bs = self.get_b(
-            minor_as, self.widths, self.depths,
-            np.array([self.rail_depth, 0, self.rail_depth, 0])
-        )
-
-        self.sections = [
-            NeckSection([NeckArea(
-                0,
-                self.scale_value(i / n, *self.widths[0::2]),
-                NeckCurve(
-                    self.scale_value(i / n, *minor_as[0::2]),
-                    self.scale_value(i / n, *minor_bs[0::2]),
-                    self.scale_value(i / n, *minor_bs[0::2] -
-                                             self.depths[0::2]),
-                    self.n0),
-                NeckCurve(
-                    self.scale_value(i / n, *minor_as[1::2]),
-                    self.scale_value(i / n, *minor_bs[1::2]),
-                    self.scale_value(i / n, *minor_bs[1::2] - self.depths[1::2]),
-                    self.n1),
-            ), NeckArea(
-                self.scale_value(i / n, *self.widths[0::2]),
-                self.scale_value(i / n, *self.widths[1::2]),
-                NeckCurve(0, 0, 0, 0),
-                NeckCurve(
-                    self.scale_value(i / n, *minor_as[1::2]),
-                    self.scale_value(i / n, *minor_bs[1::2]),
-                    self.scale_value(i / n, *minor_bs[1::2] - self.depths[1::2]),
-                    self.n1), ), ])
-            for i in reversed(range(n + 1))
-        ]
-
-        # sum of the load of all strings, Psi
-        self.load = 36.5 + 42 + 51.3 + 42.8
-        self.elastic_modulus = 10 * 10 ** 6
-        self.string_to_fretboard_distance = .125
+        self.n = n
+        self.minor_as = self.get_random_in_range(self.widths * a_max,
+                                                 self.widths * a_min)
+        self.minor_bs = None
+        self.sections = None
         self._mean_load_centroid_distance = None
         self._p1 = None
         self._p2 = None
         self._p3 = None
         self._deflection = None
-        self.n = n
+        self.set_model()
+
+    def set_model(self):
+        self.minor_bs = self.get_b(
+            self.minor_as, self.widths, self.depths,
+            np.array([self.rail_depth, 0, self.rail_depth, 0])
+        )
+        self.sections = [
+            NeckSection(self.build_areas(i), i / self.n)
+            for i in range(self.n + 1)
+        ]
 
     @property
     def p1(self):
@@ -82,8 +59,8 @@ class Neck(NeckConfig):
     def deflection(self):
         if self._deflection is None:
             self._deflection = [
-                self.curvature(i, section)
-                for i, section in enumerate(self.sections)
+                self.curvature(section)
+                for section in self.sections
             ]
         return self._deflection
 
@@ -108,29 +85,29 @@ class Neck(NeckConfig):
         """
         moments = np.array([section.second_moment
                             for section in self.sections], dtype='float')
-        xs = np.linspace(0, 1, self.n+1, dtype='float') * SCALE_LENGTH / 2
+        xs = np.linspace(0, 1, self.n+1, dtype='float') * self.scale / 2
         fit = polyfit(xs, moments, 2)
         return list(fit)
 
-    def curvature(self, x, section):
+    def curvature(self, section):
         """Find deflection of neck at position _x.
 
-        :param x:
-        :type x:
         :param section:
         :type section:
         :return:
         :rtype:
         """
-        m = (self.string_to_fretboard_distance - section.y_bar) * self.load
-        return (m / self.elastic_modulus) * (
-            ((2 * self.p1 * x + self.p2) *
-             atan((2 * self.p1 * x + self.p2) /
-                  sqrt(4 * self.p1 * self.p3 - self.p2 ** 2))) /
-            (self.p1 * sqrt(4 * self.p1 * self.p3 - self.p2 ** 2)) -
-            log(x * (self.p1 * x + self.p2) + self.p3) / (2 * self.p1)
-            # x
-        )
+        def f(y, x):
+            return 1 / (x ** 2 * self.p1 + x * self.p2 + self.p3)
+
+        return (((self.string_to_fretboard_distance - section.y_bar) *
+                 self.load / self.elastic_modulus) *
+                dblquad(f, 0, self.scale / 2 * section.z,
+                        lambda x: 0, lambda x: self.scale / 2 * section.z)[0])
+
+    @staticmethod
+    def scale_value(x, a, b):
+        return a + (b - a) * x
 
     @staticmethod
     def get_random_in_range(minimum, maximum):
@@ -146,6 +123,15 @@ class Neck(NeckConfig):
         """
         return minimum + np.random.random() * (maximum - minimum)
 
+    @staticmethod
+    def get_b(a, w, d, y):
+        """Get value for major axis a at nut, given a value for minor axis b.
+
+        :return:
+        :rtype:
+        """
+        return a * np.sqrt(1 / ((a - w) * (a + w))) * (a - d - y)
+
     def get_random_n(self):
         """Generate random exponent.
 
@@ -155,21 +141,56 @@ class Neck(NeckConfig):
         """
         return self.get_random_in_range(2, 3)
 
-    def get_b(self, a, w, d, y):
-        """Get value for major axis a at nut, given a value for minor axis b.
+    def build_areas(self, i):
+        areas = list()
 
-        :return:
-        :rtype:
-        """
-        return a * np.sqrt(1 / ((a - w) * (a + w))) * (a - d - y)
+        outer_rail = self.scale_value(i / self.n, *self.widths[0::2])
+        inner_rail = self.scale_value(i / self.n, *self.widths[1::2])
 
-    @staticmethod
-    def scale_value(x, a, b):
-        return a + (b - a) * x
+        outer_curve = NeckCurve(
+            self.scale_value(i / self.n, *self.minor_as[0::2]),
+            self.scale_value(i / self.n, *self.minor_bs[0::2]),
+            self.scale_value(i / self.n,
+                             *self.minor_bs[0::2] - self.depths[0::2]),
+            self.n0)
+        inner_curve = NeckCurve(
+            self.scale_value(i / self.n, *self.minor_as[1::2]),
+            self.scale_value(i / self.n, *self.minor_bs[1::2]),
+            self.scale_value(i / self.n,
+                             *self.minor_bs[1::2] - self.depths[1::2]),
+            self.n1)
+
+        edge_area = NeckArea(
+            inner_rail, outer_rail, outer_curve, NeckCurve(1, 0, 0, 2),
+        )
+        rib_area = NeckArea(  # inner rail
+            0, .25, inner_curve, NeckCurve(1, 0, 0, 2),
+        )
+
+        areas.append(edge_area)
+        areas.append(rib_area)
+
+        if outer_curve.y(0) < -.75 - 1e-8:
+            break_x = outer_curve.x(-.75)
+            print('break_x:', break_x)
+            areas.append(NeckArea(
+                0,
+                break_x,
+                NeckCurve(0, 0, -.75, 0),
+                inner_curve))
+            areas.append(NeckArea(
+                break_x, inner_rail,
+                outer_curve, inner_curve))
+        else:
+            areas.append(NeckArea(
+                0, inner_rail,
+                outer_curve, inner_curve))
+
+        return areas
 
 
 if __name__ == '__main__':
-    neck = Neck(10)
-    # for _ in neck.sections:
-    #     print('A:', _.area, 'y:', _.y_bar, 'I:', _.second_moment, )
+    neck = Neck()
+    for _ in neck.sections:
+        print('A:', _.area, 'y:', _.y_bar, 'I:', _.second_moment)
     print(np.array(neck.deflection))
