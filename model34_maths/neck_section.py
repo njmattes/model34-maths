4a891a8c7e73e67626cf15bd9d4540856209a0b4
Nathan Matteson <matteson@obstructures.org>
2020-02-29 09:32:20 -0600

    Second moment of section calculated around centroid of section

--- model34_maths/neck_section.py
+++ model34_maths/neck_section.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from scipy.integrate import dblquad
 from model34_maths.neck_area import NeckArea
 from model34_maths.neck_curve import NeckCurve
 
 
 class NeckSection(object):
-    def __init__(self, areas):
+    def __init__(self, areas, z):
         self.areas = areas
         self._area = None
         self._y_bar = None
@@ -13,6 +14,7 @@ class NeckSection(object):
         self._second_moment = None
         self.x_min = min([area.x_min for area in self.areas])
         self.x_max = max([area.x_max for area in self.areas])
+        self.z = z
 
     @property
     def area(self):
@@ -37,10 +39,18 @@ class NeckSection(object):
     @property
     def second_moment(self):
         if self._second_moment is None:
-            self._second_moment = sum([
-                2 * (_.second_moment + _.area * (_.y_bar - self.y_bar) ** 2)
-                for _ in self.areas])
+            self._second_moment = 2 * sum([
+                self.get_second_moment_of_area(area) for area in self.areas
+            ])
         return self._second_moment
+
+    def get_second_moment_of_area(self, area):
+        def f(y, x):
+            return (y - self.y_bar) ** 2
+
+        return dblquad(f, area.x_min, area.x_max,
+                       lambda x: area.y_min(x),
+                       lambda x: area.y_max(x))[0]
 
 
 if __name__ == '__main__':
@@ -50,6 +60,6 @@ if __name__ == '__main__':
     y1 = NeckCurve(0, 0, 0, 0)
     area1 = NeckArea(.75, 1, y0, y1)
     section = NeckSection([area0, area1])
-    print(section.area)
-    print(section.y_bar)
-    print(section.second_moment)
+    print('A:', section.area)
+    print('y_bar:', section.y_bar)
+    print('I:', section.second_moment)
