8ad275bb998b35a98860e63aa97a64da43243da6
Nathan Matteson <matteson@obstructures.org>
2020-10-08 06:09:43 -0500

    Reorganize initial guess, add constraint for curve overlapping

--- model34_maths/optimizer.py
+++ model34_maths/optimizer.py
@@ -6,17 +6,35 @@ from scipy.optimize import minimize
 from model34_maths.neck import Neck
 
 
+X0 = np.array([
+    # x[2] <= rand * x[0] + x[1] <= x[3]
+    [.5, 1.5, 0, 20, ],       # b nut inner
+    [2, 1.5, 0, 20, ],        # b nut outer
+    [.5, 1.5, 0, 20, ],           # b octave inner
+    [2, 1.5, 0, 20, ],            # b octave outer
+    [2, 2, 2, 3.5, ],               # r nut inner
+    [1, 2, 2, 3.5, ],               # r nut outer
+    [2, 2, 2, 3.2, ],               # r octave inner
+    [1.5, 2, 2.5, 3.2, ],             # r octave outer
+    [.125, .125, .125, .5, ],     # rail width
+    [.03125, .03125, 0, .125, ],  # shell
+    [.0625, .5, .5, .625, ],      # nut depth
+    [.0625, .8125, .8125, 1, ],   # octave depth
+    # [.75 + .0625, .0625, .75 + .0625, .75 + 4 * .0625],  # octave depth
+    [.0625, .0625, .0625, .125, ],  # inner rib
+])
+x0 = np.random.ranf(X0.shape[0]) * X0[:, 0] + X0[:, 1]
+
+
 class OptimalNeck(object):
     def __init__(self, neck):
         self.neck = neck
-        self.x0 = np.random.ranf(11) * \
-                  np.array([
-                      1.5, 1.5, 1.5, 1.5, 1, 1, .125, .03125, .0625, .0625,
-                      .125]) + \
-                  np.array([
-                      2, .5, 2, .5, 2, 2, .125, .03125, .5, .75 + .0625, .125])
-        self.target_area = 5.0634014771373765  # Weight with full inner rib
-        self.target_area = 3.93  # Weight with open inner rib
+        self.x0 = x0
+        # self.target_area = 5.0634014771373765  # Weight with full inner rib
+        self.target_area = 3.68  # Weight with open inner rib
+        # self.target_area = 3.68  # Weight with open inner rib
+        # self.target_area = 3.93  # Weight with open inner rib??????
+        # self.target_area = 8.542788824745898  # New curve fitting for sections
         self.target_deflection = .191
 
     def __call__(self, *args, **kwargs):
@@ -24,31 +42,44 @@ class OptimalNeck(object):
 
     @property
     def bounds(self):
-        return np.array([
-            [self.neck.widths[0], self.neck.widths[0] * 6],
-            [self.neck.widths[1], self.neck.widths[1] * 2],
-            [self.neck.widths[2], self.neck.widths[2] * 6],
-            [self.neck.widths[3], self.neck.widths[3] * 2],
-            [2, 3],  # Outer curvature
-            [2, 4],  # Inner curvature
-            [.125, .5],  # Rail
-            [.0, .125],  # Shell thickness
-            [.5, .625],  # Depth at nut
-            [.75 + .0625, .75 + 4 * .0625],  # Depth at body
-            [.0625, .125],  # Inner rib
-            # [.0625, .1875],
-        ])
+        bounds = X0[:, 2:]
+        bounds[0] = self.neck.widths[0, 0] * np.array([1, 4])
+        bounds[1] = self.neck.widths[0, 1] * np.array([1, 6])
+        bounds[2] = self.neck.widths[1, 0] * np.array([1, 4])
+        bounds[3] = self.neck.widths[1, 1] * np.array([1, 6])
+        return bounds
 
     @property
     def constraints(self):
-        return dict(type='ineq', fun=self.get_area)
-            #      fun=self.get_deflection),
+        return [
+            # Area less than target
+            dict(type='ineq', fun=self.get_area),
+            # inner faces inside outer faces
+            dict(type='ineq', fun=self.reasonable_curves),
+            # nut inner b > nut depth - shell
+            dict(type='ineq', fun=lambda x: x[1] - (x[10] - x[9])),
+            # nut outer b > nut depth
+            dict(type='ineq', fun=lambda x: x[1] - x[10]),
+            # octave inner b > .75 thickness of neck
+            dict(type='ineq', fun=lambda x: x[2] - .75),
+            # octave outer b > octave depth
+            dict(type='ineq', fun=lambda x: x[3] - x[11]),
+        ]
 
     def get_area(self, x):
         self.set_neck(x)
-        scalar = .95
-        diff = self.target_area * scalar - sum([s.area for s in self.neck.sections])
+        scalar = .9
+        total_area = sum([s.area for s in self.neck.sections])
+        diff = self.target_area * scalar - total_area
         return diff
+
+    def reasonable_curves(self, x):
+        self.set_neck(x)
+        reasonable_thickness = .1
+        xs = np.linspace(0, self.neck.nut_width - x[8], 20)
+        return np.min(
+            self.neck.get_inner_curve(0).y(xs) -
+            (self.neck.get_outer_curve(0).y(xs) + reasonable_thickness))
 
     def get_deflection(self, x):
         self.set_neck(x)
@@ -56,30 +87,24 @@ class OptimalNeck(object):
 
     def set_neck(self, x):
         self.neck.reset()
-        self.neck.minor_bs[0] = x[0]
-        self.neck.minor_bs[1] = x[1]
-        self.neck.minor_bs[2] = x[2]
-        self.neck.minor_bs[3] = x[3]
-        self.neck.r_outer = x[4]
-        self.neck.r_inner = x[5]
-        self.neck.rail_width = x[6]
-        self.neck.nut_shell = x[7]
-        self.neck.octave_shell = .0625 + x[7]
-        self.neck.nut_depth = x[8]
-        self.neck.octave_depth = x[9]
-        self.neck.inner_rib = x[10]
-        # self.neck.inner_rib = x[8]
-        self.neck.set_model(medial_rib=True)
+        self.neck.endpoint_bs = x[0:4].reshape((2, 2))
+        self.neck.exponents = x[4:8].reshape((2, 2))
+        self.neck.rail_width = x[8]
+        self.neck.nut_shell = x[9]
+        # self.neck.octave_shell = .0625 + x[9]
+        self.neck.octave_shell = x[11] - .75 + x[9]
+        self.neck.nut_depth = x[10]
+        self.neck.octave_depth = x[11]
+        self.neck.inner_rib = x[12]
+        self.neck.set_model()
 
     def objective(self, x):
         self.set_neck(x)
-        return (
-            # (sum([s.area for s in self.neck.sections]) / self.target_area) *
-            # 100 * (self.neck.deflection[-1]) ** 2
-            np.sum(
-                np.arange(len(self.neck.deflection)) *
-                np.array(self.neck.deflection) ** 2)
-        )
+        return 10 * np.sum(
+            np.arange(len(self.neck.deflection)) *
+            np.array(self.neck.deflection) ** 2)
+        # (sum([s.area for s in self.neck.sections]) / self.target_area) *
+        # 100 * (self.neck.deflection[-1]) ** 2
 
     def minimize(self):
         return minimize(
